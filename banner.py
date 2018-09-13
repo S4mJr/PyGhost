2e508d9875a773e26d487b0fc50791ca28c5b833
Sam Junior <40306119+samjunior416@users.noreply.github.com>
2018-09-14 00:12:27 +0200

    Update banner.py

--- banner.py
+++ banner.py
@@ -9,5 +9,4 @@ ban = ('''\n      :::::::::  :::   :::  ::::::::  :::    :::  ::::::::   :::::::
 ###           ###     ########  ###    ###  ########   ########     ### \033[m          
 
        Python Backdoor Generator with Ngrok | By: Sam Junior ~ @un00mz
-		t.me/universidadehacker | t.me/UniversalHacking
-		 https://github.com/universidadehacker/PyGhost''')
+		 github.com/S4mJr/PyGhost''')
